8f1512850756b2ba9e551b9f06da21e09a9f7c25
Alex Knight <alex.knight@leavittpartners.com>
2019-07-05 10:18:32 -0600

    Fixing Card images not displaying

--- frontend/california-speed/src/components/Card.jsx
+++ frontend/california-speed/src/components/Card.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-const pathToCards = require.context('../../public/card-images/', true);
 
 class Card extends React.Component {
 
-    async getCardURL() {
+    getCardURL() {
         let card = this.props.card;
-
-        let uri = pathToCards(`card-images/${card.rank}${card.suit}.png`, true);
-        return uri;
+        return `${process.env.PUBLIC_URL}/card_images/${card.rank}${card.suit}.png`;
     }
 
     render() {
