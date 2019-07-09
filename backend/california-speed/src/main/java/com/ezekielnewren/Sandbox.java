9295a963d29879a11c4e8638e487fcfd8051c732
Ezekiel Newren <ezekielnewren@gmail.com>
2019-07-09 17:11:36 -0600

    org.json example

--- backend/california-speed/src/main/java/com/ezekielnewren/Sandbox.java
+++ backend/california-speed/src/main/java/com/ezekielnewren/Sandbox.java
@@ -1,10 +1,16 @@
 package com.ezekielnewren;
 
+import org.json.JSONObject;
+
 public class Sandbox {
 
     public static void main(String[] args) {
 
-        System.out.println("hello world");
+        JSONObject x = new JSONObject();
+
+        x.put("card", "s12");
+
+        System.out.println(x.toString());
 
     }
 
